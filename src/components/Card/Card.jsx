4d3d944668fd784f1d332efb70315399bc5bb65a
Sena Balioğlu <127862385+senabalioglu@users.noreply.github.com>
2025-09-08 16:34:48 +0300

    Categories from API
    Made the categories come from the API and changed the design of the category cards.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,18 +3,6 @@ import "./Card.css";
 function Card({ bookItem, navigateFunc }) {
   return (
     <>
-    {/*
-      <div className="book-container">
-        <img className="book-img" src={bookItem.imgUrl} />
-        <div className="inner-container">
-          <div>
-            <h3 className="book-title">{bookItem.title}</h3>
-            <p className="book-author">{bookItem.author}</p>
-          </div>
-          <p className="book-price">{bookItem.price} ₺ </p>
-        </div>
-      </div>
-      */}
 
       <div onClick={navigateFunc} className="book read">
         <div className="cover">
--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -1,39 +1,35 @@
+import { useEffect, useState } from "react";
 import "./Categories.css";
 
-function Categories(){
+function Categories() {
+  const [categoriesData, setCategoriesData] = useState([]);
+  const apiUrl = import.meta.env.VITE_API_URL;
 
+  useEffect(() => {
+    fetch(`${apiUrl}/Library/categories`)
+      .then((res) => res.json())
+      .then((data) => {
+        if (Array.isArray(data)) {
+          setCategoriesData(data);
+        } else {
+          console.warn("API array dönmedi:", data);
+          setCategoriesData([]); // güvenlik için boş array
+        }
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
-    const categories = [
-        "Action",
-        "Novel",
-        "Computer",
-        "History",
-        "Hobby",
-        "Sport",
-        "Science",
-        "Reference Books",
-        "Children's Books",
-        "Religion",
-        "Literature",
-        "Humor",
-        "Philosophy",
-        "Culture",
-        "Politics",
-    ]
-
-    return(
-        <>
-        <div className="category-list" >
-            {
-                categories.map((category, index) => (
-                    <div key={index} className="category-container" >
-                        <h3 className="category-text" > {category} </h3>
-                    </div>
-                ))
-            }            
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div className="category-list">
+        {categoriesData.map((category) => (
+          <div key={category.id} className="category-container">
+            <h3 className="category-text">{category.categoryName}</h3>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default Categories
+export default Categories;
