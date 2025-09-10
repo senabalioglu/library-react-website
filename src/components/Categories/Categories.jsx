630ae252489cf24e482753c35215dd49baab3dbb
Sena Balioğlu <127862385+senabalioglu@users.noreply.github.com>
2025-09-10 18:00:14 +0300

    Sub Categories
    Implemented subcategory listing next to each category.

--- src/components/Categories/Categories.jsx
+++ src/components/Categories/Categories.jsx
@@ -4,6 +4,8 @@ import "./Categories.css";
 function Categories() {
   const [categoriesData, setCategoriesData] = useState([]);
   const apiUrl = import.meta.env.VITE_API_URL;
+  const [visible, setVisible] = useState(false);
+  const [selectedCategoryId, setSelectedCategoryId] = useState("");
 
   useEffect(() => {
     fetch(`${apiUrl}/Library/categories`)
@@ -19,15 +21,31 @@ function Categories() {
       .catch((err) => console.log(err));
   }, []);
 
+  const toggleSCVisible = (categoryId) => {
+    setSelectedCategoryId(categoryId);
+    setVisible(!visible);
+    console.log("is visible?", { visible });
+  };
   return (
     <>
       <div className="category-list">
         {categoriesData.map((category) => (
-          <div key={category.id} className="category-container">
+          <div onClick={ () => toggleSCVisible(category.id)} key={category.id} className="category-container">
             <h3 className="category-text">{category.categoryName}</h3>
           </div>
         ))}
       </div>
+      {visible && selectedCategoryId && (
+      <div className="sub-categories-list">
+        {categoriesData
+          .find((category) => category.id === selectedCategoryId)
+          ?.subCategories.map((sc, index) => (
+            <div className="category-container" key={index}>
+              <h3 className="category-text" >{sc.split("p")[1].trim()}</h3>
+            </div>
+          ))}
+      </div>
+    )}
     </>
   );
 }
